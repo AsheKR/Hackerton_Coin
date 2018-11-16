91c55cec75efbb4d6bb39492b7823c59f29758a4
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-11-15 23:00:55 -0800

    index ajax 추가

--- app/coin/views.py
+++ app/coin/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+import json
 
+import requests
+from django.shortcuts import render
+from django.urls import reverse
 
 def get_index(request):
-    pass
+    base_url = 'http://127.0.0.1:8000'
+    btc_coinValue = requests.get(base_url+reverse('apis:apis_get_coinValue', kwargs={'pk': 1}))
+    ten_coins = requests.get(base_url+reverse('apis:apis_get_ten_coin'))
+
+    ten_coins_dict = json.loads(ten_coins.content)
+    btc_coinValue_dict = json.loads(btc_coinValue.content)
+    print(btc_coinValue_dict)
+
+    context = {
+        'ten_coins': ten_coins_dict,
+        'btc_coinValue': btc_coinValue_dict,
+    }
+
+    return render(request, 'index.html', context=context)
