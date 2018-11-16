d1b2bddbc84b095722c684750bf86bff18f88f95
ElonMoon <elonmoon@gmail.com>
2018-11-16 14:41:15 +0900

    river done

--- app/coin/apis/urls.py
+++ app/coin/apis/urls.py
@@ -5,4 +5,5 @@ from coin.apis import apis as views
 urlpatterns = [
     path('list/', views.CoinCompare.as_view()),
     path('list/<int:pk>/', views.CoinDetail.as_view()),
+    path('river/', views.RiverView.as_view()),
 ]
