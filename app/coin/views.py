35be662461a76dbd2d04913e9655bcb8e6698ef9
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-11-16 00:16:43 -0800

    한강 물 온도 view 작성

--- app/coin/views.py
+++ app/coin/views.py
@@ -8,14 +8,16 @@ def get_index(request):
     base_url = 'http://127.0.0.1:8000'
     btc_coinValue = requests.get(base_url+reverse('apis:apis_get_coinValue', kwargs={'pk': 1}))
     ten_coins = requests.get(base_url+reverse('apis:apis_get_ten_coin'))
+    river_temp = requests.get(base_url+reverse('apis:get_river'))
 
     ten_coins_dict = json.loads(ten_coins.content)
     btc_coinValue_dict = json.loads(btc_coinValue.content)
-    print(btc_coinValue_dict)
+    river_temp = json.loads(river_temp.content)
 
     context = {
         'ten_coins': ten_coins_dict,
         'btc_coinValue': btc_coinValue_dict,
+        'river_temp': river_temp,
     }
 
     return render(request, 'index.html', context=context)
