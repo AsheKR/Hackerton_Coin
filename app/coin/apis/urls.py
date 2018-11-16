ee6a38f7e3ab5394b5d7836ee2eff5ebf977807f
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-11-15 22:06:24 -0800

    urls 추가

--- app/coin/apis/urls.py
+++ app/coin/apis/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from coin.apis import apis as views
 
+app_name = 'apis'
+
 urlpatterns = [
-    path('coins', views.GetTenCoinView.as_view()),
-    path('list/<int:pk>/', views.CurrentCoinValueView.as_view()),
-    path('river/', views.RiverView.as_view()),
+    path('coins', views.GetTenCoinView.as_view(), name="apis_get_ten_coin"),
+    path('list/<int:pk>/', views.CurrentCoinValueView.as_view(), name='apis_get_coinValue'),
+    path('river/', views.RiverView.as_view(), name='get_river'),
 ]
